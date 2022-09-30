7fbb0e1ea10af5597505e19b0a838439f0dbf363
wtoong <rlaxodnd5@naver.com>
2022-10-01 01:25:52 +0900

    organize import

--- src/main/java/com/example/demo/domain/Video.java
+++ src/main/java/com/example/demo/domain/Video.java
@@ -1,10 +1,14 @@
 package com.example.demo.domain;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
-import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data

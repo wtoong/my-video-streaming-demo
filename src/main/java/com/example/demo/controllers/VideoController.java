f27ea248812d7580f22dad31e88a30cc93ef6555
wtoong <rlaxodnd5@naver.com>
2022-12-17 12:40:42 +0900

    Add Security

--- src/main/java/com/example/demo/controllers/VideoController.java
+++ src/main/java/com/example/demo/controllers/VideoController.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,11 @@ public class VideoController {
 	@Autowired
     private VideoService videoService;
     
-	@GetMapping("/")
+	@GetMapping("/list")
 	public String index(Model model) {
 		List<VideoVO> allVideos = videoService.getAllVideo();
 		model.addAttribute("allVideos", allVideos);
-		return "index";
+		return "list";
 	}
 	
 	@GetMapping("/player")
